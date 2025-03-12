51ef8c3d1acb782eedc2d1e105a334f2b46b0721
siwei.wu <wusiwei2022@163.com>
2025-03-12 05:54:47 +0000

    set column name check and add document for harmonise.coloc.data

--- R/harmonise.data.R
+++ R/harmonise.data.R
@@ -2,11 +2,23 @@
 #' 
 #' Harmonise 2 GWAS summary data primarily for conventional coloc analysis.
 #' By default, the RSID, EA, and NEA will be referred to the first data set.
-#' 
+#' @param data.a The first gwas summary data set you want to harmonise. Trait, rsid, ea, and nea must be available in the gwas summary while eaf is optional. 
+#' @param data.b The second gwas summary data set you want to harmonise. Trait, rsid, ea, and nea must be available in the gwas summary while eaf is optional. 
+#' @return A harmonised data with 2 gwas summary data.
 #' @export
 harmonise.coloc.data = function(data.a, data.b){
-  trait.a = unique(data.a$trait)
-  trait.b = unique(data.b$trait)
+  if("trait" %in% names(data.a) & "trait" %in% names(data.b)){
+    trait.a = unique(data.a$trait)
+    trait.b = unique(data.b$trait)}
+  else{
+    stop("trait should be available in both 2 gwas dataset")
+  }
+  ## Check rsid, ea, and nea 
+  if(!{"rsid" %in% names(data.a)} | !{"ea" %in% names(data.a)} | !{"nea" %in% names(data.a)}){
+    stop("rsid, ea, and nea must be provided in the 1st GWAS summary to harmonise")}
+  if(!{"rsid" %in% names(data.b)} | !{"ea" %in% names(data.b)} | !{"nea" %in% names(data.b)}){
+    stop("rsid, ea, and nea must be provided in the 2nd GWAS summary to harmonise")}
+  ## Message
   message(paste0("harmonise ", trait.a, " and ", trait.b))
   ## merge trait 1 and trait 2
   colnames(data.a)[2:length(colnames(data.a))] = paste0(colnames(data.a)[2:length(colnames(data.a))], ".a")
@@ -18,17 +30,15 @@ harmonise.coloc.data = function(data.a, data.b){
   coloc.data.1 = coloc.data.1 %>% mutate(ea = ea.a, nea = nea.a, .after = rsid) %>% select(-c("ea.a", "nea.a", "ea.b", "nea.b"))
   ### subset of data where effect allele in trait 1 matches non-effect allele in trait b and non-effect allele in trait 1 matches effect allele in trait 2  
   coloc.data.2 = coloc.data %>% filter(ea.a == nea.b & nea.a == ea.b)
-  coloc.data.2 = coloc.data.2 %>% mutate(beta.b = - beta.b, eaf.b = 1-eaf.b)
+  coloc.data.2 = coloc.data.2 %>% mutate(beta.b = - beta.b)
+  if("eaf.b" %in% names(coloc.data.2)){coloc.data.2 = coloc.data.2 %>% mutate(eaf.b = 1-eaf.b)}
   coloc.data.2 = coloc.data.2 %>% mutate(ea = ea.a, nea = nea.a, .after = rsid) %>% select(-c("ea.a", "nea.a", "ea.b", "nea.b"))
   ### combine harmonised data
   coloc.data.12 = rbind(coloc.data.1, coloc.data.2)
-  ### subset of data where effect allele or non-effect allele are not matched
+  ### subset of data where effect allele or non-effect allele are incompatible
   coloc.data.3 = coloc.data %>% filter(!{rsid %in% coloc.data.12$rsid})
-  ### combine harmonised data
-  coloc.data.12 = rbind(coloc.data.1, coloc.data.2)
-  ### subset of data where effect allele or non-effect allele are not matched
-  coloc.data.3 = coloc.data %>% filter(!{rsid %in% coloc.data.12$rsid})
-  if(dim(coloc.data.3)[1] > 0){message(paste0(dim(coloc.data.3)[1], "unharmonised variants are dropped"))}
+  
+  if(dim(coloc.data.3)[1] > 0){message(paste0(dim(coloc.data.3)[1], " variants have incompatible effect allele and non-effect allele"))}
   ### output
   return(coloc.data.12)
 }
@@ -38,12 +48,21 @@ harmonise.coloc.data = function(data.a, data.b){
 #' Harmonise across 3 GWAS summary data primarily for multi-trait colocalization analysis.
 #' By default, the RSID, EA, and NEA will be referred to the first data set.
 #' 
+#' @param data.a The first gwas summary data set you want to harmonise. Trait, rsid, ea, and nea must be available in the gwas summary while eaf is optional. 
+#' @param data.b The second gwas summary data set you want to harmonise. Trait, rsid, ea, and nea must be available in the gwas summary while eaf is optional.
+#' @param data.c The second gwas summary data set you want to harmonise. Trait, rsid, ea, and nea must be available in the gwas summary while eaf is optional.
+#' @return A harmonised data with 3 gwas summary data.
 #' @export
 harmonise.moloc.data = function(data.a, data.b, data.c){
-  trait.a = unique(data.a$trait)
-  trait.b = unique(data.b$trait)
-  trait.c = unique(data.c$trait)
-  message(paste0("harmonise ", trait.a, " ", trait.b, ", and ", trait.c))
+  # if("trait" %in% names(data.a) & "trait" %in% names(data.b) & "trait" %in% names(data.c)){
+  #   trait.a = unique(data.a$trait)
+  #   trait.b = unique(data.b$trait)
+  #   trait.c = unique(data.c$trait)
+  #   message(paste0("harmonise ", trait.a, " ", trait.b, ", and ", trait.c))
+  #   }
+  # else{
+  #   stop("trait should be available in all the 3 gwas dataset")
+  # }
   ## harmonise trait.a and trait.b
   coloc.data.ab = harmonise.coloc.data(data.a, data.b)
   ## harmonise trait.a and trait.c
